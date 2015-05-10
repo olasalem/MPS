29987445d236236f9fa05db01c02400eab11f8ed
hagerrady13 <hagerrady133@gmail.com>
2015-05-10 15:58:34 +0200

    Update controlunit.cpp

--- mps_parser/controlunit.cpp
+++ mps_parser/controlunit.cpp
@@ -12,19 +12,60 @@ void ControlUnit::Reset()
     MemtoReg = false;
     MemWrite = false;
     ALUSrc = false;
-    RegWrite = false;
+    RegWrite = true;
     ALUOp = 0;
 }
-void ControlUnit::Set(bool t1 = false, bool t2 = false, bool t3 = false, bool t4 = false, bool t5 = false, bool t6 = false, bool t7 = false, int t8 = 0)
+void ControlUnit::Set(inst Curr_Inst)
 {
-    RegDst = t1;
-    Branch = t2;
-    MemRead = t3;
-    MemtoReg = t4;
-    MemWrite = t5;
-    ALUSrc = t6;
-    RegWrite = t7;
-    ALUOp = t8;
+    Reset();   // Initially reset all
+    //RegDst & ALUSrc
+   if(Curr_Inst.imm==0){
+       RegDst = 1;
+       ALUSrc = 1;
+   }
+   // Branch
+   if(Curr_Inst.jAddress){
+       Branch = 1;
+       RegWrite = 0;
+   }
+   //MemRead & MemReg
+   if(Curr_Inst.instType == "LW" ){
+       MemRead = MemtoReg = 1;
+       RegWrite = 1;
+   }
+   //MemWrite & RegWrite
+   if(Curr_Inst.instType == "SW"){
+       MemWrite = 1;
+       RegWrite = 0;
+   }
+   //ALUOp
+   if(Curr_Inst.instType == "ADD" || uff1.Curr_Instruction.instType == "ADDI" ){   // 1 = Addition
+       ALUOp = 1;
+   }
+   else if(Curr_Inst.instType == "XOR") {  // 2 = XORing
+       ALUOp = 2;
+   }
+   else if(Curr_Inst.instType == "SW" || Curr_Inst.instType == "LW") {  // 3 = Store Word or Load Word , u execute either one by checking other signals
+       ALUOp = 3;
+   }
+   else if(Curr_Inst.instType == "SLT") {  // 4 = SLT
+       ALUOp = 4;
+   }
+   else if(Curr_Inst.instType == "BLE") {  // 5 = BLE
+       ALUOp = 5;
+   }
+   else if(Curr_Inst.instType == "J") {  // 6 = Jump
+       ALUOp = 6;
+   }
+   else if(Curr_Inst.instType == "JAL") {  // 7 = Jump and Link
+       ALUOp = 7;
+   }
+   else if(Curr_Inst.instType == "JR") {  // 8 = JR
+       ALUOp = 8;
+   }
+   else {
+       ALUOp = 0;           // Invalid Instruction
+   }
 }
 ControlUnit::~ControlUnit()
 {
