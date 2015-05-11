ae5f1a5dc46deddb4b01eacec2b70d4272ca8f2b
hagerrady13 <hagerrady133@gmail.com>
2015-05-11 12:33:44 +0200

    Update Mem_WB.h

--- mps_parser/Mem_WB.h
+++ mps_parser/Mem_WB.h
@@ -5,20 +5,12 @@ class Mem_WB
 {
 public:
 	Mem_WB();
-	void Read_Result(int &R1,int &R2,int &R3){
+/*	void Read_Result(int &R1,int &R2,int &R3){
 		R1 = Reg_Result;
 		R2 = Mem_Result;
 		R3 = Reg_destination;
 	}
-	void Set_Result(int R1=0,int R2=0,int R3=0){
-		Reg_Result = R1;
-		Mem_Result = R2;
-		Reg_destination = R3;
-		if (Reg_Result==0)
-			WBReg = 1;
-		if (Mem_Result == 0)
-			WBMem = 1;
-	}
+	void Set_Result(int R1=0,int R2=0,int R3=0)*/
 	~Mem_WB();
 	int Mem_Result, Reg_Result;      // results to be saved in the Reg file and the Data memory
 	int Reg_destination;            // stores the Destination Reg
